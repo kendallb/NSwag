9e3c5d92d2108ea082da4586d41351cd091e8470
Kendall Bennett <kendallb@amain.com>
2020-08-10 17:24:11 -0700

    Added unit tests to test for correct behaviour in NJsonSchema project for oneOf and anyOf

---
+++ src/NSwag.CodeGeneration.CSharp.Tests/AnyOfTests.cs
@@ -0,0 +1,139 @@
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NSwag.CodeGeneration.CSharp.Tests
+{
+    public class AnyOfTests
+    {
+        [Fact]
+        public async Task Correct_class_generated_for_anyOf()
+        {
+            //// Arrange
+            var swagger = @"{
+  ""openapi"": ""3.0.0"",
+            ""info"": {
+                ""title"": ""oneOf bug"",
+                ""version"": ""1.0""
+            },
+            ""components"": {
+                ""schemas"": {
+                    ""request_body"": {
+                        ""type"": ""object"",
+                        ""additionalProperties"": false,
+                        ""anyOf"": [
+                        {
+                            ""$ref"": ""#/components/schemas/first_id_request""
+                        },
+                        {
+                            ""$ref"": ""#/components/schemas/second_id_request""
+                        }
+                        ]
+                    },
+                    ""first_id_request"": {
+                        ""type"": ""object"",
+                        ""additionalProperties"": false,
+                        ""properties"": {
+                            ""first_id"": {
+                                ""type"": ""string""
+                            }
+                        }
+                    },
+                    ""second_id_request"": {
+                        ""type"": ""object"",
+                        ""additionalProperties"": false,
+                        ""properties"": {
+                            ""second_id"": {
+                                ""type"": ""string""
+                            }
+                        }
+                    }
+                }
+            }
+        }";
+            var document = await OpenApiDocument.FromJsonAsync(swagger);
+
+            //// Act
+            var settings = new CSharpClientGeneratorSettings { ClassName = "MyClass" };
+            var generator = new CSharpClientGenerator(document, settings);
+            var code = generator.GenerateFile();
+
+            //// Assert
+            Assert.Contains(@"public partial class Request_body 
+    {
+        [Newtonsoft.Json.JsonProperty(""first_id"", Required = Newtonsoft.Json.Required.DisallowNull, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public string First_id { get; set; }
+    
+        [Newtonsoft.Json.JsonProperty(""second_id"", Required = Newtonsoft.Json.Required.DisallowNull, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public string Second_id { get; set; }
+    
+    
+    }".Replace("\r\n", "\n"), code);
+        }
+        [Fact]
+        public async Task Correct_class_generated_for_anyOf_nullable()
+        {
+            //// Arrange
+            var swagger = @"{
+  ""openapi"": ""3.0.0"",
+            ""info"": {
+                ""title"": ""oneOf bug"",
+                ""version"": ""1.0""
+            },
+            ""components"": {
+                ""schemas"": {
+                    ""request_body"": {
+                        ""type"": ""object"",
+                        ""additionalProperties"": false,
+                        ""anyOf"": [
+                        {
+                            ""$ref"": ""#/components/schemas/first_id_request""
+                        },
+                        {
+                            ""$ref"": ""#/components/schemas/second_id_request""
+                        }
+                        ]
+                    },
+                    ""first_id_request"": {
+                        ""type"": ""object"",
+                        ""additionalProperties"": false,
+                        ""properties"": {
+                            ""first_id"": {
+                                ""type"": ""string"",
+                                ""nullable"": true
+                            }
+                        }
+                    },
+                    ""second_id_request"": {
+                        ""type"": ""object"",
+                        ""additionalProperties"": false,
+                        ""properties"": {
+                            ""second_id"": {
+                                ""type"": ""string"",
+                                ""nullable"": true
+                            }
+                        }
+                    }
+                }
+            }
+        }";
+            var document = await OpenApiDocument.FromJsonAsync(swagger);
+
+            //// Act
+            var settings = new CSharpClientGeneratorSettings { ClassName = "MyClass" };
+            var generator = new CSharpClientGenerator(document, settings);
+            var code = generator.GenerateFile();
+
+            //// Assert
+            Assert.Contains(@"public partial class Request_body 
+    {
+        [Newtonsoft.Json.JsonProperty(""first_id"", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public string First_id { get; set; }
+    
+        [Newtonsoft.Json.JsonProperty(""second_id"", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public string Second_id { get; set; }
+    
+    
+    }".Replace("\r\n", "\n"), code);
+        }
+    }
+}
